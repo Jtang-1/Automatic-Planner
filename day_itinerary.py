534d21befee769179fe344243d8966a4720dc2c8
Jonathan Tang <88759246+JonathanTang-738@users.noreply.github.com>
2021-10-11 18:10:40 -0700

    implemented shortest distance V2, starts from home to farthest location, then greedy algorithm to rest of places. Leaves home in vertices

---
+++ day_itinerary.py
@@ -0,0 +1,5 @@
+class DayItinerary:
+    def __init__(self, locations: []):
+        self.locations = locations
+
+
--- shortest_path.py
+++ shortest_path.py
@@ -1,40 +1,75 @@
 from website import modify_graph
 from place_model.place import Place
+from place_model.home import Home
+from day_itinerary import DayItinerary
 import copy
 
 graph = modify_graph.get_graph()
 graph_copy = copy.copy(graph)
 
-def find_shortest_path() -> []:
+
+def create_day_itinerary():
     home = graph.home
-    total_path_connections = graph_copy.num_vertices - 1
-    visit_order = [home]
-    current_place = home
-    for _ in range(total_path_connections):
-        print("in for loop")
-        next_place = find_closest_neighbor(current_place)
-        graph_copy.remove_vertex(current_place)
-        visit_order.append(next_place)
-        current_place = next_place
-    for count, place in enumerate(visit_order):
-        print(count, place.name)
+    print("home is", home.name)
+    farthest_neighbor = farthest_not_home_neighbor(home)
+    print("farthest neighbor is", farthest_neighbor.name)
+    visit_order = [home, farthest_neighbor]
+
+    visit_order = visit_order + shortest_route_from(farthest_neighbor)
+    print([(count, place.name) for count, place in enumerate(visit_order)])
     return visit_order
 
 
-def find_closest_neighbor(node: Place) -> Place:
+def shortest_route_from(node: Place) -> []:
+    total_path_connections = graph_copy.num_vertices - 2
+    visit_order = []
+    current_place = node
+    for _ in range(total_path_connections):
+        print("in for loop")
+        next_place = closest_not_home_neighbor(current_place)
+        graph_copy.remove_vertex(current_place)
+        visit_order.append(next_place)
+        current_place = next_place
+    # for count, place in enumerate(visit_order):
+    #     print(count, place.name)
+    return visit_order
+
+
+def closest_not_home_neighbor(node: Place) -> Place:
     print("vertices of copies", list(graph_copy.vertices))
-    print("passed in node", node)
+    print("passed in node", node.name)
     neighbors_edges = graph_copy.neighbors_edges(node)
     closest_neighbor_dist = float('inf')
     closest_neighbor = None
     for edge in neighbors_edges:
         if edge.distance > closest_neighbor_dist:
             continue
-        closest_neighbor_dist = edge.distance
-        if edge.place1 != node:
+        if edge.place1 != node and not isinstance(edge.place1, Home):
             closest_neighbor = edge.place1
-        else:
+        elif edge.place2 != node and not isinstance(edge.place2, Home):
             closest_neighbor = edge.place2
-    print(edge.place2.name, edge.distance)
-    print(edge.place1.name, edge.distance)
+        else:
+            continue
+        closest_neighbor_dist = edge.distance
+
+    print("closest neighbor", closest_neighbor.name,  closest_neighbor_dist)
     return closest_neighbor
+
+
+def farthest_not_home_neighbor(node: Place) -> Place:
+    neighbors_edges = graph_copy.neighbors_edges(node)
+    farthest_neighbor_dist = 0
+    farthest_neighbor = None
+    for edge in neighbors_edges:
+        if edge.distance < farthest_neighbor_dist:
+            continue
+        if edge.place1 != node and not isinstance(edge.place1, Home):
+            farthest_neighbor = edge.place1
+        elif edge.place2 != node and not isinstance(edge.place2, Home):
+            farthest_neighbor = edge.place2
+        else:
+            continue
+        farthest_neighbor_dist = edge.distance
+
+    print("farthest neighbor", farthest_neighbor.name, farthest_neighbor_dist)
+    return farthest_neighbor
